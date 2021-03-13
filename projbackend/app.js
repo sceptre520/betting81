9a338faff41cfe01ca248690db71fc8aebac3f87
declanwalpole <declan.walpole@gmail.com>
2021-03-13 13:38:00 -0700

    Add files via upload

---
+++ projbackend/app.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentBRoutes");
+const teamRoutes = require("./routes/team");
+const matchRoutes = require("./routes/match");
+const externalMatchRoutes = require("./routes/externalMatch.js");
+const marketRoutes = require("./routes/market");
+const playerRoutes = require("./routes/player");
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
+app.use("/api", teamRoutes);
+app.use("/api", matchRoutes);
+app.use("/api", marketRoutes);
+app.use("/api", externalMatchRoutes);
+app.use("/api", playerRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
