d790fde45bd2cd5003fb0bd7607158dd68c7828d
declanwalpole <declan.walpole@gmail.com>
2021-03-13 13:54:49 -0700

    Add files via upload

---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Redirect, Link } from "react-router-dom";
+
+import { signin, authenticate, isAutheticated } from "../auth/helper";
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "declanwalpole2@declan.com",
+    password: "123345",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAutheticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, loading: false });
+        } else {
+          authenticate(data, () => {
+            setValues({
+              ...values,
+              didRedirect: true,
+            });
+          });
+        }
+      })
+      .catch(console.log("signin request failed"));
+  };
+
+  const performRedirect = () => {
+    if (didRedirect) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div className="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                onChange={handleChange("email")}
+                value={email}
+                className="form-control"
+                type="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                value={password}
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign In" description="Only signed in users can view odds">
+      {loadingMessage()}
+      {errorMessage()}
+      {signInForm()}
+      {performRedirect()}
+
+      <p className="text-white text-center font-italic font-weight-light">
+        <Link to="/signup" className="text-center text-warning">
+          If you do not have an account already, click here to sign up.
+        </Link>
+      </p>
+    </Base>
+  );
+};
+
+export default Signin;
